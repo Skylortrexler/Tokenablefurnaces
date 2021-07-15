9ae7f071bef1a9fb7069072fb79a14e0272a8b87
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-07-15 13:36:42 -0700

    Initial commit

---
+++ src/main/java/website/skylorbeck/minecraft/tokenablefurnaces/Tokenablefurnaces.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.minecraft.tokenablefurnaces;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tokenablefurnaces implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
